7e39f7e718836be6597088ea3efd004f59decfe2
Srdjan Colovic <srdjan.colovic@etondigital.com>
2022-10-21 22:35:49 +0200

    menu initial setup

--- src/js/script.js
+++ src/js/script.js
@@ -25,7 +25,8 @@ navLinks.forEach((link) => {
     link.addEventListener('click', togglerFn);
 });
 
-const swiper = new Swiper('.mySwiper', {
+//Sliders
+const landingSwiper = new Swiper('.landing-swiper', {
     speed: 1500,
     loop: true,
     autoplay: {
@@ -33,6 +34,31 @@ const swiper = new Swiper('.mySwiper', {
         disableOnInteraction: false,
     },
     effect: 'fade',
+});
+
+const menu = new Swiper('.menu-swiper', {
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'fraction',
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 4,
+            spaceBetween: 40,
+        },
+        1024: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+        },
+    },
 });
 
 //On load
