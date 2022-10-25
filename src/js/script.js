f72460d692cf047f3d8fd3298b89fdb5553a8366
Srdjan Colovic <srdjan.colovic@etondigital.com>
2022-10-25 20:04:47 +0200

    menu finished

--- src/js/script.js
+++ src/js/script.js
@@ -46,14 +46,15 @@ const menu = new Swiper('.menu-swiper', {
         prevEl: '.swiper-button-prev',
     },
     breakpoints: {
+        0: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+        },
         640: {
             slidesPerView: 2,
             spaceBetween: 20,
         },
-        768: {
-            slidesPerView: 4,
-            spaceBetween: 40,
-        },
+
         1024: {
             slidesPerView: 3,
             spaceBetween: 20,
